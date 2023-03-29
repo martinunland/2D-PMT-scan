9a67373bd93d9496f30c37a8a8cea7e776f00d7a
Martin Antonio Unland Elorrieta <m.unland@uni-muenster.de>
2023-03-29 18:18:23 +0200

    adding docstrings of minimiser and pmt_centre

--- src/pmt_centre_finder/minimiser.py
+++ src/pmt_centre_finder/minimiser.py
@@ -1,3 +1,15 @@
+"""
+minimiser.py
+
+This module contains the PMT_circle_fitting class, which is used to find the 
+center of a Photomultiplier Tube (PMT) by analysing the intensity profiles 
+measured at different angles.
+
+Classes:
+    - ProfileData: A dataclass that holds the measured data of a single profile.
+    - PMT_circle_fitting: A class that uses the measured intensity profiles at various 
+                          angles to fit circles and find the center of the PMT.
+"""
 from dataclasses import dataclass
 import logging
 import pathlib
@@ -23,6 +35,21 @@ class ProfileData:
 
 
 class PMT_circle_fitting:
+    """
+    A class that uses the measured intensity profiles at various angles to fit circles
+    and find the centre of the PMT.
+
+    Methods:
+        - _calculate_intensities_to_fit: Calculates the range of intensities to be interpolated for the fits.
+        - _load_data_from_files: Loads the profile data from the saved files.
+        - interpolate_profile_intensity_point: Interpolates the intensity points for a given profile.
+        - fit_profiles_and_get_PMT_centre: Performs multiple fits on the intensity profiles to find the centre of the PMT and calculates the uncertainties.
+        - get_centre_from_intensity_set: Fits the centre of the PMT for a given set of intensity levels.
+        - log_likelihood: Calculates the log-likelihood for the given model parameters.
+        - minimise: Minimises the log-likelihood function using the iminuit package.
+        - create_iminuit: Creates an iminuit object with the necessary initial parameters, limits, errors, and fixed status.
+    """
+
     def __init__(
         self,
         PMT_radius: float,
@@ -36,10 +63,10 @@ class PMT_circle_fitting:
         self.profile_angles = profile_angles
         self.NR_FITS = 20
         self.MEAN_INTENSITY_COUNTS_PER_FIT = 10
-        self._get_data_from_files()
+        self._load_data_from_files()
         self._calculate_intensities_to_fit()
 
-    def _calculate_intensities_to_fit(self):
+    def _calculate_intensities_to_fit(self) -> None:
         centre_intensity = []
         threshold = []
         for _, item in self.data.items():
@@ -49,9 +76,11 @@ class PMT_circle_fitting:
         threshold = np.mean(threshold)
         self.MIN_INTENSITY = threshold
         self.MAX_INTENSITY = centre_intensity / 2.0
-        log.debug(f"Intensities used in fits range from {self.MIN_INTENSITY:.3g} to {self.MAX_INTENSITY:.3g}")
+        log.debug(
+            f"Intensities used in fits range from {self.MIN_INTENSITY:.3g} to {self.MAX_INTENSITY:.3g}"
+        )
 
-    def _get_data_from_files(self):
+    def _load_data_from_files(self) -> None:
         self.data = {}
         for angle in self.profile_angles:
             try:
@@ -68,7 +97,21 @@ class PMT_circle_fitting:
             len(self.data) > 3
         ), f"Something went wrong, number of profiles measured ({len(self.data)}) too low (you need at least 4)!"
 
-    def interpolate_profile_intensity_point(self, angle, R, charge, intensity):
+    def interpolate_profile_intensity_point(
+        self, angle: float, R: np.ndarray, charge: np.ndarray, intensity: float
+    ) -> Tuple[float, float]:
+        """
+        Interpolate the Cartesian position of a point on the profile curve at a given intensity level.
+
+        Args:
+            angle (float): The angle of the profile curve in degrees.
+            R (np.ndarray): The radial distance from the centre to the fibre position for each intensity value.
+            charge (np.ndarray): The measured intensity values along the profile.
+            intensity (float): The intensity level at which the position should be interpolated.
+
+        Returns:
+            Tuple[float, float]: The interpolated Cartesian coordinates (x, y) of the point on the profile curve at the given intensity level.
+        """
         interpol = interp1d(charge, R)
         interp_R = interpol(intensity)
         interp_x_y = pol2cart(interp_R, np.deg2rad(angle))
@@ -77,12 +120,22 @@ class PMT_circle_fitting:
     def fit_profiles_and_get_PMT_centre(
         self,
     ) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+        """
+        Fit the circle profiles multiple times to find the PMT centre and estimate its uncertainty.
+
+        Returns:
+            Tuple[Tuple[float, float], Tuple[float, float]]: A tuple containing:
+                - the x-coordinate of the PMT centre and its error,
+                - the y-coordinate of the PMT centre and its error.
+        """
         log.info("Fitting profile data several times to avoid systematics...")
 
         all_ints = np.linspace(self.MIN_INTENSITY, self.MAX_INTENSITY, 1000)
         x0_fits = []
         y0_fits = []
-        with alive_bar(self.NR_FITS, enrich_print = False, title="Fitting profiles") as bar:
+        with alive_bar(
+            self.NR_FITS, enrich_print=False, title="Fitting profiles"
+        ) as bar:
             for _ in range(self.NR_FITS):
                 n = max(2, np.random.poisson(self.MEAN_INTENSITY_COUNTS_PER_FIT))
                 x_0, y_0 = self.get_centre_from_intensity_set(
@@ -100,8 +153,16 @@ class PMT_circle_fitting:
 
         return (mean_x0, error_x0), (mean_y0, error_y0)
 
-    def get_centre_from_intensity_set(self, intensity_levels) -> Tuple[float, float]:
+    def get_centre_from_intensity_set(self, intensity_levels:np.ndarray) -> Tuple[float, float]:
+        """
+        Fit the circle profiles for a given set of intensity levels and return the estimated PMT centre.
 
+        Args:
+            intensity_levels (np.ndarray): An array of intensity levels to use for fitting.
+
+        Returns:
+            Tuple[float, float]: The estimated x and y coordinates of the PMT centre.
+        """
         self.datasets = []
         for _ in intensity_levels:
             self.datasets.append([[], []])
@@ -118,18 +179,26 @@ class PMT_circle_fitting:
                     self.datasets[i][0].append(interp_cartesian[1])
                     self.datasets[i][1].append(interp_cartesian[0])
                 except Exception as err:
-                    print(err)
+                    log.error(err)
                     pass
         for i, _ in enumerate(intensity_levels):
             self.datasets[i][0] = np.array(self.datasets[i][0])
             self.datasets[i][1] = np.array(self.datasets[i][1])
 
-        self.minimize()
+        self.minimise()
         fit_parameters = self.m.np_values()
         return fit_parameters[-2], fit_parameters[-1]
 
-    def log_likelihood(self, *args):
+    def log_likelihood(self, *args)->float:
+        """
+        Calculate the log-likelihood of the model for the given parameters.
 
+        Args:
+            *args: The model parameters: the radii of the circles for each intensity level, followed by the x and y coordinates of the centre.
+
+        Returns:
+            float: The log-likelihood of the model.
+        """
         xc = args[-2]
         yc = args[-1]
         Rs = args[:-2]
@@ -141,13 +210,19 @@ class PMT_circle_fitting:
             totallh += -0.5 * np.sum((model) ** 2)
         return -totallh
 
-    def minimize(self):
+    def minimise(self)->None:
+        """
+        Perform the minimisation of the log-likelihood using the iminuit package.
+        """
         self.create_iminuit()
         self.m = Minuit(self.log_likelihood, name=self.parname, **self.minuit_args)
         self.m.migrad()
         self.m.hesse()
 
-    def create_iminuit(self):
+    def create_iminuit(self)->None:
+        """
+        Create an iminuit Minuit object with initial parameters, limits, errors, and fixed parameter settings.
+        """
         initial = {}
         limits = {}
         fix = {}
--- src/pmt_centre_finder/pmt_centre_finder.py
+++ src/pmt_centre_finder/pmt_centre_finder.py
@@ -1,3 +1,21 @@
+"""
+pmt_centre_finder.py
+
+This module contains the CentreFinder class, which is responsible for finding the centre of a PMT using intensity measurements at various angles (edge profiles).
+
+Classes:
+- CentreFinder: Main class that controls the process of finding the PMT centre.
+- measure_intensity: Measures the intensity level of the PMT output and its error at the current fibre position.
+- prompt_to_user: Prompts the user for manual input of (x,y,z) values to place the fibre near the PMT centre.
+- _input_user_value: Handles the user input of (x,y,z) values or 'q' to quit.
+- _warn_current_position: Logs a warning with the current position of the motors.
+- measure_background_threshold: Measures the background intensity threshold and sets the signal threshold accordingly.
+- measure_profile: Measures the intensity profile for a given angle (polar coordinates, from cfg.profile_r_start to cfg.profile_r_stop).
+- measure_all_profiles: Measures the intensity profiles for all specified angles.
+- analyse_profiles: Analyses the measured profiles and finds the centre of the PMT using the PMT_circle_fitting class.
+- run: Main function to execute the PMT centre finding process.
+"""
+import asyncio
 import logging
 from typing import Tuple
 import numpy as np
@@ -11,8 +29,15 @@ from src.helper import make_folder_in_working_directory
 
 log = logging.getLogger(__name__)
 
+
 class CentreFinder:
-    def __init__(self, cfg: CentreFindConfig, motors: MotorsControl, analyser: DataAnalysis, daq: DAQDevice):
+    def __init__(
+        self,
+        cfg: CentreFindConfig,
+        motors: MotorsControl,
+        analyser: DataAnalysis,
+        daq: DAQDevice,
+    ) -> None:
 
         self.cfg = cfg
         self.analyser = analyser
@@ -20,64 +45,99 @@ class CentreFinder:
         self.daq = daq
         self.save_path = make_folder_in_working_directory("centre_finder_output")
 
-    async def measure_intensity(self)->Tuple[float,float]:
+    async def measure_intensity(self) -> Tuple[float, float]:
+        """
+        Measure the intensity and error of the signal from the measurement device (DAQ).
+
+        Returns:
+            Tuple[float, float]: A tuple containing the intensity and error of the measured signal.
+        """
         data, timestamp = await self.daq.read()
         (intensity, error), _ = self.analyser.get_simple_intensity(data)
         return intensity, error
 
-    async def prompt_to_user(self):
+    async def prompt_to_user(self) -> None:
+        """
+        Prompt the user to position the fibre near the PMT centre. The user can input x, y, and z values to move the motors.
+        They can continue inputting values until they believe the fibre is close enough to the PMT centre.
+        """
         log.warning("You want to find the centre of the PMT!")
-        log.warning("For this to work you have to make an effort and place the fibre already near the PMT centre (at least within ~10-20mm)...")
-        log.warning("If this is already the case, write q/Q. Otherwise, feed me with (x,y,z) values until you think it is around the centre")
+        log.warning(
+            "For this to work you have to make an effort and place the fibre already near the PMT centre (at least within ~10-20mm)..."
+        )
+        log.warning(
+            "If this is already the case, write q/Q. Otherwise, feed me with (x,y,z) values until you think it is around the centre"
+        )
         log.critical("Be careful, I can't attemp avoiding collisions during this!")
         await self._input_user_value()
 
-    async def _input_user_value(self):
+    async def _input_user_value(self) -> None:
         log.warning("Enter three values, separated by spaces, or y/Y")
         while True:
             self._warn_current_position()
-            user_input = input("x y z or q/Q:")
+            user_input = await asyncio.to_thread(input, "x y z or q/Q:")
             if "q" in user_input or "Q" in user_input:
-                return 
+                return
             try:
                 x, y, z = user_input.split()
                 x, y, z = float(x), float(y), float(z)
                 await self.motors.mot.move_to_absolute_position_in_mm([x, y, z])
 
             except ValueError as err:
-                log.error("You have to provide 3 numbers separated by a space, or q to quit!")
+                log.error(
+                    "You have to provide 3 numbers separated by a space, or q to quit!"
+                )
 
-    def _warn_current_position(self):
-        x,y,z = self.motors.get_current_position()
+    def _warn_current_position(self) -> None:
+        x, y, z = self.motors.get_current_position()
         log.warning(f"Current position x: {x:.2f}, y: {y:.2f}, z: {z:.2f}")
 
-
-    async def measure_background_threshold(self, factor:float = 3):
-        """Move the fibre to a location where PMT is not illuminated and measure the background level. The threshold for signal is set as factor*background.
-        Args:
-            factor (default 3)
+    async def measure_background_threshold(self, factor: float = 3) -> None:
         """
-        #Look for a radius/angle pair that is within motor volume
-        for radius in np.arange(self.cfg.PMT_bulb_radius+10, self.cfg.PMT_bulb_radius+5, -1):
-            for phi in np.arange(0,360,1):
+        Move the fibre to a location where PMT is not illuminated and measure the background level. The threshold for signal is set as factor*background.
+
+        Args:
+            factor (float, optional): The factor used to set the threshold for the signal. Default value is 3.
+        """
+        # Look for a radius/angle pair that is within motor volume
+        for radius in np.arange(
+            self.cfg.PMT_bulb_radius + 10, self.cfg.PMT_bulb_radius + 5, -1
+        ):
+            for phi in np.arange(0, 360, 1):
                 if self.motors.check_position_polar(radius, phi):
                     break
         await self.motors.check_PMT_curvature_and_move_polar(radius, phi)
         self.threshold = await self.measure_intensity()
-        self.threshold = self.threshold[0]*factor
+        self.threshold = self.threshold[0] * factor
 
-    async def measure_profile(self, angle):
-        #start far away from PMT taking into account beam diameter
+    async def measure_profile(self, angle: float) -> Tuple[float, float, float, float]:
+        """
+        Measure the intensity profile along a given angle. The method starts far away from the PMT and moves closer in steps, checking the intensity at each step.
+
+        Args:
+            angle: The angle along which to measure the intensity profile.
+        Returns:
+            Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: A tuple containing:
+            - the measured intensities at each step,
+            - the associated errors,
+            - the positions of the fibre at each step,
+            - the radial distance from the initial position to the fibre position.
+        """
+        # start far away from PMT taking into account beam diameter
         current_R = self.cfg.profile_r_start
         await self.motors.check_PMT_curvature_and_move_polar(current_R, angle)
-        
+
         intensity = await self.measure_intensity()
 
-        if intensity[0]>self.threshold:
-            log.info(f"Skipping profile along angle {angle}; first value already over threshold...")
-            log.info("If you keep getting this message, run CentreFinder.measure_background_threshold(factor) again with a larger factor")
+        if intensity[0] > self.threshold:
+            log.info(
+                f"Skipping profile along angle {angle}; first value already over threshold..."
+            )
+            log.info(
+                "If you keep getting this message, run CentreFinder.measure_background_threshold(factor) again with a larger factor"
+            )
             return
-        
+
         not_in_cathode = True
         intensities = []
         intensities_e = []
@@ -87,13 +147,15 @@ class CentreFinder:
         while True:
             current_position = self.motors.get_current_position()
             intensity = await self.measure_intensity()
-            log.info(f"Intensity at R {current_R:.1f}, phi {angle:.1f}: {intensity[0]:.3g}+-{intensity[1]:.3g}")   
-            intensities.append(intensity[0]) 
-            intensities_e.append(intensity[1]) 
-            position.append(current_position) 
+            log.info(
+                f"Intensity at R {current_R:.1f}, phi {angle:.1f}: {intensity[0]:.3g}+-{intensity[1]:.3g}"
+            )
+            intensities.append(intensity[0])
+            intensities_e.append(intensity[1])
+            position.append(current_position)
             position_R.append(current_R)
 
-            if not_in_cathode and np.abs(intensity[0])>self.threshold:
+            if not_in_cathode and np.abs(intensity[0]) > self.threshold:
                 current_R += self.cfg.mid_step
                 step = -self.cfg.fine_step
                 not_in_cathode = False
@@ -102,36 +164,66 @@ class CentreFinder:
             if current_R < self.cfg.profile_r_stop:
                 break
             await self.motors.check_PMT_curvature_and_move_polar(current_R, angle)
-                
-        return(np.array(intensities), np.array(intensities_e), np.array(position), np.array(position_R))
 
+        return (
+            np.array(intensities),
+            np.array(intensities_e),
+            np.array(position),
+            np.array(position_R),
+        )
 
+    async def measure_all_profiles(self) -> None:
+        np.warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+        self.all_profile_angles = np.arange(0, 360, self.cfg.ang_step)
 
-    async def measure_all_profiles(self):
-        np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
-        self.all_profile_angles = np.arange(0,360, self.cfg.ang_step)
-
-        await self.motors.check_PMT_curvature_and_move_polar(0,0)
+        await self.motors.check_PMT_curvature_and_move_polar(0, 0)
         centre_intensity = await self.measure_intensity()
 
-        with alive_bar(self.all_profile_angles.size, enrich_print = False, title="Measuring profiles") as bar:
+        with alive_bar(
+            self.all_profile_angles.size, enrich_print=False, title="Measuring profiles"
+        ) as bar:
             for angle in self.all_profile_angles:
                 try:
-                    intensity, intensity_error, positions, Rs = await self.measure_profile(angle)
-                    np.save(self.save_path / f"profile_{angle}deg.npy", (intensity, intensity_error, positions, Rs, centre_intensity, self.threshold))
+                    (
+                        intensity,
+                        intensity_error,
+                        positions,
+                        Rs,
+                    ) = await self.measure_profile(angle)
+                    np.save(
+                        self.save_path / f"profile_{angle}deg.npy",
+                        (
+                            intensity,
+                            intensity_error,
+                            positions,
+                            Rs,
+                            centre_intensity,
+                            self.threshold,
+                        ),
+                    )
                     bar()
                 except Exception as err:
                     log.error(err)
                     bar(skipped=True)
                     continue
-                
 
-    def analyse_profiles(self):
-        fitter = PMT_circle_fitting(self.cfg.PMT_bulb_radius, self.motors.PMT_centre, self.save_path, self.all_profile_angles)
+    def analyse_profiles(self) -> None:
+        """
+        Analyse the measured intensity profiles to find the PMT centre using the PMT_circle_fitting class.
+        """
+        fitter = PMT_circle_fitting(
+            self.cfg.PMT_bulb_radius,
+            self.motors.PMT_centre,
+            self.save_path,
+            self.all_profile_angles,
+        )
         (x0, x0_err), (y0, y0_err) = fitter.fit_profiles_and_get_PMT_centre()
 
-
-    async def run(self):
+    async def run(self) -> None:
+        """
+        Executes the PMT centre finding process by configuring the DAQ, prompting the user for input to move fibre, measuring the background threshold,
+        measuring all intensity profiles, and analyzing the profiles to find the PMT centre.
+        """
         await self.daq.configure_for_primary()
         await self.prompt_to_user()
         await self.measure_background_threshold()
